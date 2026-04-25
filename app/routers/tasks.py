a5e5736a3da784715d6e8dfc3384078697ac7754
Red-14 <red-14@gmail.com>
2026-04-25 20:26:34 +0300

    refactor: add status code

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -9,19 +9,19 @@ router = APIRouter(prefix="/tasks", tags=["tasks"])
 
 
 # Эндпоинт для создания новой задачи
-@router.post("/", response_model=Task)
+@router.post("/", response_model=Task, status_code=status.HTTP_201_CREATED)
 async def create_task(task: TaskCreate):
     return task_service.create_task(task)
 
 
 # Эндпоинт для получения списка всех задач
-@router.get("/", response_model=list[Task])
+@router.get("/", response_model=list[Task], status_code=status.HTTP_200_OK)
 async def get_tasks():
     return task_service.get_tasks()
 
 
 # Эндпоинт для получения задачи по её ID
-@router.get("/{task_id}", response_model=Task)
+@router.get("/{task_id}", response_model=Task, status_code=status.HTTP_200_OK)
 async def get_task(task_id: int):
     try:
         return task_service.get_task(task_id)
